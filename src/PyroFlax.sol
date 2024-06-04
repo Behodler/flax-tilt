797e4512cbb39f4d3b6ea2c8034859d5d3fc88f9
gititgoro <admin@behodler.io>
2024-06-04 12:56:53 +0200

    removed outdated packages

--- src/PyroFlax.sol
+++ src/PyroFlax.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@superfluid/contracts/superfluid/SuperToken.sol";
 import "@superfluid/contracts/interfaces/superfluid/ISuperToken.sol";
 import "@superfluid/contracts/interfaces/superfluid/ISuperfluid.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 // Explanation: Identical to original PyroFlax ERC20 but renamed to avoid clashes with openzeppelin
 import "./PyroERC20.sol";
